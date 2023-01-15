906ff2c88f9f80408256d86de51f39df2582c4b6
Sora-tabata <sora48691525@gmail.com>
2023-01-15 20:30:06 +0900

    20230115

--- batch.sh
+++ batch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 conda activate colmap0
-dir_path="/home/sora-lab/dataset/data_kitti/*"
+dir_path="/home/sora-desktop/dataset/data_kitti/*"
 dirs=`find $dir_path -maxdepth 0 -type d`
 
 for dir in $dirs;
@@ -11,11 +11,21 @@ do
     CURRENT=$(cd $(dirname $0);pwd)
     DIR_NAME=`echo "$CURRENT" | sed -e 's/.*\/\([^\/]*\)$/\1/'`
     echo $DIR_NAME
-    cp $dir/KeyFrameTrajectory.txt
-    cp $dir/reconstruction
+    cp $dir/OUTPUT_DROIDSLAM/poses_1.npy $dir/poses.npy
+    cp $dir/OUTPUT_DROIDSLAM/tstamps_1.npy $dir/tstamps.npy
+    mkdir $dir/output
+    mkdir $dir/output/opted
     rm -rf image_2
     rm -rf image_3
     rm -rf matches
     rm -rf exif
-    rm -rf 
+    rm -rf undistorted
+    rm -rf features
+    rm -rf reports
 done
+for dir in $dirs;
+do
+    cd $dir
+    python /home/sora-desktop/Documents/estimate_trajectory_kitti/main.py &
+done
+wait
